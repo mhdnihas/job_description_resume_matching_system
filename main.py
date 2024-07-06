f734927be1dc1ca60dd089860bf0d3e90cd17e0b
Muhammed Nihas <muhammednihas8589@gmail.com>
2024-07-06 13:37:45 +0530

    creat matcher function

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 from flask import Flask, request, render_template
+import os
+import PyPDF2
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER']='uploads/'
+
+def extract_text_pdf(file_path):
+    text=""
+    with open(file_path,'rb') as file:
+        reader=PyPDF2.PdfReader(file)
+        for page in reader.pages:
+            text+=page.extract_text()
+    return text
+
+def extract_text(file_path):
+    if file_path.endwith('.pdf'):
+        return extract_text_pdf(file_path)
+    else:
+        return ""
+
+
+
+
 
 @app.route("/")
 def matchresume():
     return render_template('matchresume.html')
+
+
+@app.route("/matcher",methods=['GET','POST'])
+def matcher():
+    if request.method=='POST':
+        job_description=request.form.get('job-description')
+        resume_files=request.form.getlist('resumes')
+        resumes=[]
+        for resume_file in resume_files:
+            filename=os.path.join(app.config['UPLOAD_FOLDER'],resume_file.filename)
+            resume_file.save(filename)
+            resumes.append(extract_text(filename))
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
